744f53917c281218745e9515b3480ec64206a86c
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-24 18:46:32 +0200

    adds links to navbar

--- test/src/layout/Navbar.js
+++ test/src/layout/Navbar.js
@@ -4,8 +4,10 @@ import {Link} from "react-router-dom"
 function Navbar() {
     return (
         <div className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/heroes">Heroes</Link>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
