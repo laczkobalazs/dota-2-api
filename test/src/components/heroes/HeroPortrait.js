6122326acb2b6c13d60ccc4631d933af787b7ab5
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-25 11:16:42 +0200

    adds functions to give back hero portraits

--- test/src/components/heroes/HeroPortrait.js
+++ test/src/components/heroes/HeroPortrait.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {AxiosInstance as axios} from "axios";
 
-function HeroPortrait({ hero }) {
+export function HeroPortrait({ hero }) {
     const  heroImgUrlStart = "https://api.opendota.com/apps/dota2/images/heroes/"
 
     function getSmallHeroImg({ hero }) {
@@ -12,16 +12,11 @@ function HeroPortrait({ hero }) {
         return heroImgUrlStart + hero + "_sb.png";
     }
 
+    function getFullHeroImg({ hero }) {
+        return heroImgUrlStart + hero + "_full.png";
+    }
 
+    function getHeroIcon({ hero }) {
 
-
-    const [heroPortrait, setHeroPortrait] = useState()
-    //const [initialUrl, setInitialUrl] = useState("http://cdn.dota2.com/apps/dota2/images/heroes/")
-    const [initialUrl, setInitialUrl] = useState("https://api.opendota.com/apps/dota2/images/heroes/")
-    //https://api.opendota.com/apps/dota2/images/heroes/antimage_full.png?%22
-    useEffect(
-        axios.get(initialUrl + hero + "_sb.png").then(
-
-        )
-    )
+    }
 }
