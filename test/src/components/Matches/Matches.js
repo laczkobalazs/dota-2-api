4921df8c8d562d05c054beee6ff5b6532ac48022
Jorakasi <gabor.olah0323@gmail.com>
2020-06-25 06:25:06 +0200

    design changes

--- test/src/components/Matches/Matches.js
+++ test/src/components/Matches/Matches.js
@@ -16,13 +16,14 @@ const Matches = props => {
     }, [])
 
 
-
     return (
         <ul>
             <div className="listed-matches">
                 {matches.map((m) => {
-                    return <li style={{padding: '5px'}} key={m.match_id}> {m.radiant_name} <span> vs. </span> {m.dire_name}
-                    <Link to={"/match/" + m.match_id } key ={m.match_id}> {m.match_id} </Link>
+                    return <li style={{padding: '5px'}} key={m.match_id}> {m.radiant_name}
+                        <span> vs. </span> {m.dire_name}
+                        <br/>
+                        | <Link to={"/match/" + m.match_id} key={m.match_id}> {m.match_id} </Link> |
                     </li>
                 })}
             </div>
