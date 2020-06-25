e1be1113a6a58cab8a3746840e88da9439df5d85
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-25 12:20:05 +0200

    cleans up Card

--- test/src/layout/Card.js
+++ test/src/layout/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link, Route} from "react-router-dom";
-import { HeroPortrait } from "../components/heroes/HeroPortrait"
+import {HeroPortrait} from "../components/heroes/HeroPortrait"
 
 function Card({hero, heroName}) {
     let heroNameStr = Object.values({hero}).toString().toLowerCase().replace("npc_dota_hero_", '');
@@ -12,7 +12,7 @@ function Card({hero, heroName}) {
 
                 </Link>
                 <img src={"http://cdn.dota2.com/apps/dota2/images/heroes/" + heroNameStr + "_lg.png"} alt="no-img"/>
-                <img src={HeroPortrait( heroNameStr, "small")} alt="no-img" />
+                <img src={HeroPortrait(heroNameStr, "small")} alt="no-img"/>
             </div>
         </div>
     )
