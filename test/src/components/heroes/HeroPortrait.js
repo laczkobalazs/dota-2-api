2155854e0cb20161409a5ef67d2949c56c0ddd5a
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-25 11:14:45 +0200

    adds heroportrait functional comp

--- test/src/components/heroes/HeroPortrait.js
+++ test/src/components/heroes/HeroPortrait.js
@@ -1,11 +1,27 @@
 import React, {useEffect, useState} from "react";
 import {AxiosInstance as axios} from "axios";
 
-function getHeroPortrait({hero}) {
-    const [heroPortrait, setHeroPortrait] = useState()
-    const [initialUrl, setInitialUrl] = useState("http://cdn.dota2.com/apps/dota2/images/heroes/")
+function HeroPortrait({ hero }) {
+    const  heroImgUrlStart = "https://api.opendota.com/apps/dota2/images/heroes/"
 
+    function getSmallHeroImg({ hero }) {
+        return heroImgUrlStart + hero + "_sb.png";
+    }
+
+    function getLargeHeroImg({ hero }) {
+        return heroImgUrlStart + hero + "_sb.png";
+    }
+
+
+
+
+    const [heroPortrait, setHeroPortrait] = useState()
+    //const [initialUrl, setInitialUrl] = useState("http://cdn.dota2.com/apps/dota2/images/heroes/")
+    const [initialUrl, setInitialUrl] = useState("https://api.opendota.com/apps/dota2/images/heroes/")
+    //https://api.opendota.com/apps/dota2/images/heroes/antimage_full.png?%22
     useEffect(
-        axios.get(initialUrl).then()
+        axios.get(initialUrl + hero + "_sb.png").then(
+
+        )
     )
 }
