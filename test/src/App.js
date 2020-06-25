7b97a6aea8bb2f99aa4a0ead4d37826a60ac2a96
laczkobalazs <laczko.balazs89@gmail.com>
2020-06-25 13:53:21 +0200

    adds HeroDetails.js

--- test/src/App.js
+++ test/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./layout/Navbar";
+import HeroDetails from "./components/heroes/HeroDetails"
 
 
 function App() {
@@ -9,7 +10,11 @@ function App() {
       <div className="background">
           <Router>
               <Navbar/>
+              <Route path="/heroes/:heroName" component={HeroDetails}>
+
+              </Route>
           </Router>
+
       </div>
   );
 }
---
+++ test/src/components/heroes/HeroDetails.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
+
+function HeroDetails() {
+    const [heroStats, setHeroStats] = useState([])
+    let {heroName} = useParams()
+    useEffect(
+
+    )
+
+}
+
+export default HeroDetails;
